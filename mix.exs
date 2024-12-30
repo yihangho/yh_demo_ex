c3bd8c1f96fca15180f7210ff6c19110b6e0d802
Yihang Ho <hoyihang5@gmail.com>
2024-12-30 19:21:24 +0800

    Prepare for publication

--- mix.exs
+++ mix.exs
@@ -7,7 +7,11 @@ defmodule YHDemo.MixProject do
       version: "0.1.0",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      description: description(),
+      deps: deps(),
+      package: package(),
+      source_url: "https://github.com/yihangho/yh_demo_ex",
+      homepage_url: "https://github.com/yihangho/yh_demo_ex",
     ]
   end
 
@@ -23,6 +27,20 @@ defmodule YHDemo.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp description() do
+    "Yihang's demo Elixir library"
+  end
+
+  defp package() do
+    [
+      name: "yh_demo",
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/yihangho/yh_demo_ex",
+      },
     ]
   end
 end
