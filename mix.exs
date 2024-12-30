b9ebe08ea175dfc6fddbc826037921252ac1af83
Yihang Ho <hoyihang5@gmail.com>
2024-12-30 19:55:00 +0800

    Relax Elixir version to 1.16

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule YHDemo.MixProject do
     [
       app: :yh_demo,
       version: "0.1.0",
-      elixir: "~> 1.17",
+      elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       description: description(),
       deps: deps(),
